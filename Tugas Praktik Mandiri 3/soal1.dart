5401f5c005f39ed0fea2bc81d036029d4dd8cc00
Nurul Aziz <nurulaziz803@gmail.com>
2023-06-25 19:08:01 +0700

    tambah task 3

---
+++ Tugas Praktik Mandiri 3/soal1.dart
@@ -0,0 +1,17 @@
+void main() {
+  int nilai = 39;
+
+  if (nilai >= 70) {
+    print("A");
+  } else {
+    if (nilai >= 40) {
+      print("B");
+    } else {
+      if (nilai >= 0) {
+        print("C");
+      } else {
+        print("Nilai Kosong");
+      }
+    }
+  }
+}
